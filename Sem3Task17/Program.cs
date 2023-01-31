ecab1a865f4593801a337107b5163540347b5125
Alina <alinakislowa98@gmail.com>
2023-01-31 15:15:44 +0700

    добавили задачи c 17 по 23

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+// выдаёт номер четверти плоскости, в которой 
+// находится эта точка.
+
+int coordX = ReadData("Введите координату X:");
+int coordY = ReadData("Введите координату Y:");
+PrintQuterTest();
+
+// метод читает данные пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод определяет четверть по координатам точки
+void PrintQuterTest()
+{
+    if (coordX > 0 && coordY > 0) Console.WriteLine("Точка в четверти 1");
+    if (coordX > 0 && coordY < 0) Console.WriteLine("Точка в четверти 2");
+    if (coordX < 0 && coordY < 0) Console.WriteLine("Точка в четверти 3");
+    if (coordX < 0 && coordY > 0) Console.WriteLine("Точка в четверти 4");
+}
---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Выводим результат пользователю
+void PrintData(bool msg)
+{
+    Console.WriteLine(msg);
+}
+// Проверка на полиндром
+bool PalindromTest (int n)
+{
+    bool res = false;
+    int d1 = n/10000;
+    int d2 = (n/1000)%10;
+    int d3 = (n/10)%10;
+    int d4 = n%10;
+    res = (d1==d4)&&(d2==d3)?true:false;
+    return res;
+}
+
+// выводим число
+int Num = ReadData("Введите число: ");
+// вызываем метод теста на полиндром
+bool resalt = PalindromTest (Num);
+// выводим результат
+PrintData(resalt);
